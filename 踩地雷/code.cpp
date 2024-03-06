8b40cf9445583cf3f24b37ba347fbfe7f787b1e5
Izhenn <A930413l@gmail.com>
2024-03-06 12:52:14 +0800

    Update code.cpp

--- 踩地雷/code.cpp
+++ 踩地雷/code.cpp
@@ -1,6 +1,22 @@
-//½ò¦a¹p 
+//Minesweeper 
 #include<iostream>
+#include<stdlib.h>
+#include<time.h>
 using namespace std;
 int main(){
+	int rand();
+	srand(time(NULL));
+	int a[10][10];
+//	int bomb1[2],bomb2[2],bomb3[2],bomb4[2],bomb5[2],bomb6[2],bomb7[2],bomb8[2],bomb9[2],bomb10[2];
+	int bomb[10][2];
+	for(int i=0;i<=10;i++){
+		int temp[1][1]=rand()%10+1;
+		for(int t=i;i>=0;i--){
+			if(temp[0][0]!=bomb[i][0]){
+				bomb[i][0]=rand()%10+1;
+			}
+		}
+		
+	}	
 	
 } 
